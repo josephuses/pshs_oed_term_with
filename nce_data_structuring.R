a1ff8c927d837262591200de5514ce05ec673bc4
Joseph Tabadero, Jr <josephuses@gmail.com>
2018-09-10 20:12:26 +0800

    redefined principal and alternate qualifiers

--- nce_data_structuring.R
+++ nce_data_structuring.R
@@ -10,6 +10,18 @@ ncetd <- ncedat %>% select(-total) %>%
   gather(subject, score, math:abstract)
 
 ncetd <- ncetd %>% group_by(year, subject) %>% mutate(mean = mean(score)) %>%
+  ungroup() %>%
+  group_by(year, id, lname, fname, mname) %>%
+  mutate(total = sum(score)) %>%
+  ungroup() %>%
+  group_by(year) %>%
+  mutate(total_mean = mean(total)) %>%
   mutate(pass = as.integer(score > mean)) %>% ungroup() %>%
   group_by(year, id) %>% mutate(pass = sum(pass)) %>%
-  mutate(passflag2 = case_when(pass == 4~"passed", TRUE~"failed"))
+  mutate(justpass = score < mean & score >= .9 * mean) %>%
+  mutate(passflag2 = case_when(pass == 4 & total >= total_mean~"P",
+                               pass < 4 & total >= total_mean~"A",
+                               TRUE~"F"))
+
+ncetd %>% tabyl(passflag2, year, choice1)
+
